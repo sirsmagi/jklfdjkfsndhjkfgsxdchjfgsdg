a8ec81ddfdf20a09f10cb76c43c30c3aff7737d5
sirsmagi <sirsmagi@gmail.com>
2026-05-06 15:50:50 +0300

    Add files via upload

---
+++ SoulFinder.py
@@ -0,0 +1,159 @@
+import requests
+import os
+import phonenumbers
+from phonenumbers import geocoder
+from phonenumbers import carrier
+from rich import print
+
+if os.name == "nt":
+	os.system("cls")
+else:
+	os.system("clear")
+
+
+print(r"[#00BFFF]  _________            .__  ___________.__            .___             [/#00BFFF]")
+print(r"[#00BFFF] /   _____/ ____  __ __|  | \_   _____/|__| ____    __| _/___________  [/#00BFFF]")
+print(r"[#80DFFF] \_____  \ /  _ \|  |  \  |  |    __)  |  |/    \  / __ |/ __ \_  __ \ [/#80DFFF]")
+print(r"[#80DFFF] /        (  <_> )  |  /  |__|     \   |  |   |  \/ /_/ \  ___/|  | \/ [/#80DFFF]")
+print(r"[#FFFFFF]/_______  /\____/|____/|____/\___  /   |__|___|  /\____ |\___  >__|   [/#FFFFFF]")
+print(r"[#FFFFFF]        \/                       \/            \/      \/    \/ [/#FFFFFF]")
+
+print("[#00BFFF](created by Dexter Morgan XD)")
+
+print(r"[#FFFFFF][1]Поиск[#80DFFF] по [#00BFFF]номеру ")
+print(r"[#FFFFFF][2]Поиск[#80DFFF] по [#00BFFF]username ")
+print(r"[#FFFFFF][3]Поле[#80DFFF]зные сайты по [#00BFFF]OSINT ")
+
+what = int(input("==>"))
+
+if what == 1:
+	if os.name == "nt":
+
+		os.system('cls')
+
+		print(r"[#00BFFF]  _________            .__  ___________.__            .___             [/#00BFFF]")
+		print(r"[#00BFFF] /   _____/ ____  __ __|  | \_   _____/|__| ____    __| _/___________  [/#00BFFF]")
+		print(r"[#80DFFF] \_____  \ /  _ \|  |  \  |  |    __)  |  |/    \  / __ |/ __ \_  __ \ [/#80DFFF]")
+		print(r"[#80DFFF] /        (  <_> )  |  /  |__|     \   |  |   |  \/ /_/ \  ___/|  | \/ [/#80DFFF]")
+		print(r"[#FFFFFF]/_______  /\____/|____/|____/\___  /   |__|___|  /\____ |\___  >__|   [/#FFFFFF]")
+		print(r"[#FFFFFF]        \/                       \/            \/      \/    \/ [/#FFFFFF]")
+
+		
+		print("[#00BFFF] Введите номер (+1999999999):")
+
+		numwhat = input("==>")
+
+		number = phonenumbers.parse(numwhat, None)
+
+		print(number,geocoder.description_for_number(number,"en"), carrier.name_for_number(number,"en"))
+
+		if phonenumbers.is_valid_number(number):
+			print("[#FFFFFF]Но[#80DFFF]мер ва[#00BFFF]лидный")
+		else:
+			print("[#FFFFFF]Но[#80DFFF]мер нева[#00BFFF]лидный")
+
+	if os.name == "posix":
+
+		os.system('clear')
+
+		print(r"[#00BFFF]  _________            .__  ___________.__            .___             [/#00BFFF]")
+		print(r"[#00BFFF] /   _____/ ____  __ __|  | \_   _____/|__| ____    __| _/___________  [/#00BFFF]")
+		print(r"[#80DFFF] \_____  \ /  _ \|  |  \  |  |    __)  |  |/    \  / __ |/ __ \_  __ \ [/#80DFFF]")
+		print(r"[#80DFFF] /        (  <_> )  |  /  |__|     \   |  |   |  \/ /_/ \  ___/|  | \/ [/#80DFFF]")
+		print(r"[#FFFFFF]/_______  /\____/|____/|____/\___  /   |__|___|  /\____ |\___  >__|   [/#FFFFFF]")
+		print(r"[#FFFFFF]        \/                       \/            \/      \/    \/ [/#FFFFFF]")
+
+		print("[#00BFFF] Введите номер (+1999999999):")
+
+		numwhat = input("==>")
+
+		number = phonenumbers.parse(numwhat, None)
+
+		print(number,geocoder.description_for_number(number,"en"), carrier.name_for_number(number,"en"))
+
+		if phonenumbers.is_valid_number(number):
+			print("[#FFFFFF]Но[#80DFFF]мер ва[#00BFFF]лидный")
+		else:
+			print("[#FFFFFF]Но[#80DFFF]мер нева[#00BFFF]лидный")
+
+if what	== 2:
+	if os.name == "nt":
+
+		os.system("cls")
+
+		print(r"[#00BFFF]  _________            .__  ___________.__            .___             [/#00BFFF]")
+		print(r"[#00BFFF] /   _____/ ____  __ __|  | \_   _____/|__| ____    __| _/___________  [/#00BFFF]")
+		print(r"[#80DFFF] \_____  \ /  _ \|  |  \  |  |    __)  |  |/    \  / __ |/ __ \_  __ \ [/#80DFFF]")
+		print(r"[#80DFFF] /        (  <_> )  |  /  |__|     \   |  |   |  \/ /_/ \  ___/|  | \/ [/#80DFFF]")
+		print(r"[#FFFFFF]/_______  /\____/|____/|____/\___  /   |__|___|  /\____ |\___  >__|   [/#FFFFFF]")
+		print(r"[#FFFFFF]        \/                       \/            \/      \/    \/ [/#FFFFFF]")
+
+		print("[#00BFFF] Введите username:")
+
+		whatuse = input("==>")
+
+		print("[#FFFFFF]Про[#80DFFF]верьте ссыл[#00BFFF]ки:")
+
+		print(f"https://www.facebook.com/{whatuse}")
+		print(f"https://www.instagram.com/{whatuse}")
+		print(f"https://www.tiktok.com/@{whatuse}")
+		print(f"https://t.me/{whatuse}")
+		print(f"https://www.youtube.com/@{whatuse}")
+		print(f"https://vk.com/{whatuse}")
+		print(f"https://github.com/{whatuse}")
+
+	if os.name == "posix":
+
+		os.system("clear")
+
+		print(r"[#00BFFF]  _________            .__  ___________.__            .___             [/#00BFFF]")
+		print(r"[#00BFFF] /   _____/ ____  __ __|  | \_   _____/|__| ____    __| _/___________  [/#00BFFF]")
+		print(r"[#80DFFF] \_____  \ /  _ \|  |  \  |  |    __)  |  |/    \  / __ |/ __ \_  __ \ [/#80DFFF]")
+		print(r"[#80DFFF] /        (  <_> )  |  /  |__|     \   |  |   |  \/ /_/ \  ___/|  | \/ [/#80DFFF]")
+		print(r"[#FFFFFF]/_______  /\____/|____/|____/\___  /   |__|___|  /\____ |\___  >__|   [/#FFFFFF]")
+		print(r"[#FFFFFF]        \/                       \/            \/      \/    \/ [/#FFFFFF]")
+
+		print("[#00BFFF] Введите username:")
+
+		whatuse = input("==>")
+
+		print("[#FFFFFF]Про[#80DFFF]верьте ссыл[#00BFFF]ки:")
+
+		print(f"https://www.facebook.com/{whatuse}")
+		print(f"https://www.instagram.com/{whatuse}")
+		print(f"https://www.tiktok.com/@{whatuse}")
+		print(f"https://t.me/{whatuse}")
+		print(f"https://www.youtube.com/@{whatuse}")
+		print(f"https://vk.com/{whatuse}")
+		print(f"https://github.com/{whatuse}")
+
+if what == 3:
+	if os.name	== "nt":
+
+		os.system("cls")
+
+		print(r"[#00BFFF]  _________            .__  ___________.__            .___             [/#00BFFF]")
+		print(r"[#00BFFF] /   _____/ ____  __ __|  | \_   _____/|__| ____    __| _/___________  [/#00BFFF]")
+		print(r"[#80DFFF] \_____  \ /  _ \|  |  \  |  |    __)  |  |/    \  / __ |/ __ \_  __ \ [/#80DFFF]")
+		print(r"[#80DFFF] /        (  <_> )  |  /  |__|     \   |  |   |  \/ /_/ \  ___/|  | \/ [/#80DFFF]")
+		print(r"[#FFFFFF]/_______  /\____/|____/|____/\___  /   |__|___|  /\____ |\___  >__|   [/#FFFFFF]")
+		print(r"[#FFFFFF]        \/                       \/            \/      \/    \/ [/#FFFFFF]")
+
+		print("[#00BFFF]https://geohints.com/ - сборник данных которые помогут если есть фото жертвы(на улице)")
+		print("[#00BFFF]https://osintframework.com/ - карта всех инструментов")
+		print("[#00BFFF]https://haveibeenpwned.com/ - проверка утечек данных")
+
+	if os.name	== "posix":
+
+		os.system("clear")
+
+		print(r"[#00BFFF]  _________            .__  ___________.__            .___             [/#00BFFF]")
+		print(r"[#00BFFF] /   _____/ ____  __ __|  | \_   _____/|__| ____    __| _/___________  [/#00BFFF]")
+		print(r"[#80DFFF] \_____  \ /  _ \|  |  \  |  |    __)  |  |/    \  / __ |/ __ \_  __ \ [/#80DFFF]")
+		print(r"[#80DFFF] /        (  <_> )  |  /  |__|     \   |  |   |  \/ /_/ \  ___/|  | \/ [/#80DFFF]")
+		print(r"[#FFFFFF]/_______  /\____/|____/|____/\___  /   |__|___|  /\____ |\___  >__|   [/#FFFFFF]")
+		print(r"[#FFFFFF]        \/                       \/            \/      \/    \/ [/#FFFFFF]")
+
+		print("[#00BFFF]https://geohints.com/ - сборник данных которые помогут если есть фото жертвы(на улице)")
+		print("[#00BFFF]https://osintframework.com/ - карта всех инструментов")
+		print("[#00BFFF]https://haveibeenpwned.com/ - проверка утечек данных")
